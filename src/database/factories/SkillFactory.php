916fa952be6571bf8d106669173c9409606d82eb
shion0625 <xkaito0912@gmail.com>
2022-05-11 23:59:54 +0900

    skill test update

---
+++ src/database/factories/SkillFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Skill>
+ */
+class SkillFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'category'=>$this->faker->randomNumber(2, false),
+            'language'=>$this->faker->sentence(2, true),
+            'experience'=>$this->faker->randomFloat(2, 0, 50),
+        ];
+    }
+}
