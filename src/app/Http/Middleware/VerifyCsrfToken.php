47556fc49ee89a96a6215afeee24c0a90a674608
shion0625 <xkaito0912@gmail.com>
2022-05-12 00:17:12 +0900

    php-cs-fix

--- src/app/Http/Middleware/VerifyCsrfToken.php
+++ src/app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Closure;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -15,7 +16,7 @@ class VerifyCsrfToken extends Middleware
         //
     ];
 
-    public function handle($request, \Closure $next)
+    public function handle($request, Closure $next)
     {
         if(env('APP_ENV') !== 'testing')
         {
